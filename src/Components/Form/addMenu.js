e4b7095f4b366fbc5d80ac5e993e12d88503c8b8
hussain.ihunt <hussain.ihunt@nxvt.com>
2023-11-06 18:42:11 +0500

    add form

--- src/Components/Form/addMenu.js
+++ src/Components/Form/addMenu.js
@@ -1,29 +1,42 @@
 import React from "react";
 import { useFormik } from "formik";
-import yup from yup
+import * as Yup from "yup";
+import "./form.css"
 
 const AddMenuForm = () => {
 
-    const validationSchema = yup.object({
-        reason: yup.string().required("Reason is required")
+    const validationSchema = Yup.object({
+        reason: Yup.string().required("Reason is required")
     });
 
     const formik = useFormik({
         initialValues: {
-            reason: ""
+            title: "",
+            desc: "",
+            image: "",
+            price: ""
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
-            setIsSubmitting(true);
-            const data = new FormData();
-            data.append("status", type);
-            data.append("decline_reason", values.reason);
-            dispatch(toggleFundrequestStatus(id, data, onSuccess, onError))
+
         },
     });
     return (
-        <div className="add from">
-            <form></form>
+        <div className="add-from">
+            <div className="form-header">
+                <p>Add New Dish Form</p>
+                <span>X</span>
+            </div>
+            <form>
+                <input type="text" name="title" value={formik?.values?.title} placeholder="Name of the Dish" />
+                <input type="file" name="image" value={formik?.values?.image} placeholder="image" />
+                <input type="text" name="desc" value={formik?.values?.desc} placeholder="Desc Full/Half" />
+                <input type="number" name="Price" value={formik?.values?.price} placeholder="Price of the Dish" />
+                <div className="form-btns">
+                    <button type="reset"> Reset </button>
+                    <button type="submit"> Submit</button>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -9,6 +9,7 @@ import {
 } from "react-icons/fa";
 import "./menu.css";
 import { useLocation } from "react-router-dom";
+import AddMenuForm from "../Form/addMenu";
 
 const Menu = () => {
   const [orderPage, setOrderPage] = useState(false);
@@ -16,7 +17,7 @@ const Menu = () => {
   const location = useLocation();
 
   useEffect(() => {
-    if (location?.pathname == "/new-order") {
+    if (location?.pathname === "/new-order") {
       setOrderPage(true);
     } else {
       setOrderPage(false);
@@ -24,98 +25,102 @@ const Menu = () => {
   }, [location?.pathname]);
 
   return (
-    <div className="menu-container">
-      <div className="card-container-header">
-        <p>{orderPage ? "Add Foods to Cart" : "Find The Best Food"} </p>
-        {
-          orderPage ?
-            !isNewOrder ? <p className="add-new" onClick={() => { setIsNewOrder(true) }}>New Order</p> : null
-            :
-            <p className="add-new">Add Dish</p>
-        }
-      </div>
-      <div className="card-container">
-        <div className="menu-card">
-          <div className="image-container">
-            <img src={Piza} alt="menu image" />
-            <CiHeart size={25} className="heart" />
-          </div>
-          <div className="details">
-            <div className="name-price">
-              <p className="menu-name">Chicken Menchurian</p>
-              <p className="menu-price">550</p>
+    <>
+      <div className="menu-container">
+        <div className="card-container-header">
+          <p>{orderPage ? "Add Foods to Cart" : "Find The Best Food"} </p>
+          {
+            orderPage ?
+              !isNewOrder ? <p className="add-new" onClick={() => { setIsNewOrder(true) }}>New Order</p> : null
+              :
+              <p className="add-new">Add Dish</p>
+          }
+        </div>
+        <div className="card-container">
+          <div className="menu-card">
+            <div className="image-container">
+              <img src={Piza} alt="menu" />
+              <CiHeart size={25} className="heart" />
+            </div>
+            <div className="details">
+              <div className="name-price">
+                <p className="menu-name">Chicken Menchurian</p>
+                <p className="menu-price">550</p>
+              </div>
+            </div>
+            <p className="desc">with Onion, cheese, Olive and many more</p>
+            <div className="rating-btn">
+              <span className="rating">
+                <FaStar size={15} color="#fdcc0d" className="star" />
+                5.0
+              </span>
+              {orderPage ? (
+                <button className="edit-btn">Add</button>
+              ) : (
+                <button className="edit-btn">Edit</button>
+              )}
             </div>
           </div>
-          <p className="desc">with Onion, cheese, Olive and many more</p>
-          <div className="rating-btn">
-            <span className="rating">
-              <FaStar size={15} color="#fdcc0d" className="star" />
-              5.0
-            </span>
-            {orderPage ? (
-              <button className="edit-btn">Add</button>
-            ) : (
-              <button className="edit-btn">Edit</button>
-            )}
-          </div>
         </div>
-      </div>
-      {
-        isNewOrder ?
-          <div className="order-list">
-            <p className="new-order-text">New Order</p>
-            <span className="cross" onClick={() => setIsNewOrder(false)}>
-              X
-            </span>
-            <div className="order-cart-container">
-              <div className="order-cart">
-                <div className="order-img">
-                  <img src={Piza} alt="food" />
-                </div>
-                <div className="order-details">
-                  <FaTrash size={12} className="delete" />
-                  <span className="counter">
-                    <FaRegPlusSquare size={18} className="counter-icon" />
-                    <p>01</p>
-                    <FaRegMinusSquare size={18} className="counter-icon" />
-                  </span>
-                  <p className="order-title">Chicken Noodle Soup</p>
-                  <p className="order-desc">Small Bowl</p>
-                  <div className="price-quantity">
-                    <p>Rs. 50</p>
+        {
+          isNewOrder ?
+            <div className="order-list">
+              <p className="new-order-text">New Order</p>
+              <span className="cross" onClick={() => setIsNewOrder(false)}>
+                X
+              </span>
+              <div className="order-cart-container">
+                <div className="order-cart">
+                  <div className="order-img">
+                    <img src={Piza} alt="food" />
+                  </div>
+                  <div className="order-details">
+                    <FaTrash size={12} className="delete" />
+                    <span className="counter">
+                      <FaRegPlusSquare size={18} className="counter-icon" />
+                      <p>01</p>
+                      <FaRegMinusSquare size={18} className="counter-icon" />
+                    </span>
+                    <p className="order-title">Chicken Noodle Soup</p>
+                    <p className="order-desc">Small Bowl</p>
+                    <div className="price-quantity">
+                      <p>Rs. 50</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="Pricing-details">
-              <table>
-                <tr>
-                  <th>Items</th>
-                  <th>Price</th>
-                </tr>
-                <tr className="derails-row">
-                  <td className="item-name">Centro comercial Moctezuma</td>
-                  <td className="item-price">1450</td>
-                </tr>
-                <tr className="derails-row">
-                  <td className="item-name">Centro comercial Moctezuma</td>
-                  <td className="item-price">1450</td>
-                </tr>
-                <tr className="derails-row">
-                  <td className="item-name">Centro comercial Moctezuma</td>
-                  <td className="item-price">1450</td>
-                </tr>
-                <hr></hr>
-                <tr className="derails-row">
-                  <td className="total-price-title">Total Amount</td>
-                  <td className="total-price-amount">1450</td>
-                </tr>
-              </table>
-            </div>
-          </div> : null
-      }
+              <div className="Pricing-details">
+                <table>
+                  <tr>
+                    <th>Items</th>
+                    <th>Price</th>
+                  </tr>
+                  <tr className="derails-row">
+                    <td className="item-name">Centro comercial Moctezuma</td>
+                    <td className="item-price">1450</td>
+                  </tr>
+                  <tr className="derails-row">
+                    <td className="item-name">Centro comercial Moctezuma</td>
+                    <td className="item-price">1450</td>
+                  </tr>
+                  <tr className="derails-row">
+                    <td className="item-name">Centro comercial Moctezuma</td>
+                    <td className="item-price">1450</td>
+                  </tr>
+                  <hr></hr>
+                  <tr className="derails-row">
+                    <td className="total-price-title">Total Amount</td>
+                    <td className="total-price-amount">1450</td>
+                  </tr>
+                </table>
+              </div>
+            </div> : null
+        }
 
-    </div>
+      </div>
+      <AddMenuForm />
+    </>
+
   );
 };
 
